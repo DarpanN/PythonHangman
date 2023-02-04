1b826b9fbe79efeb2d89956e84b8ee203f21e63e
DarpanN <cloversdnc1994@gmail.com>
2023-02-04 19:58:36 +0545

    Hangman Game

---
+++ hangman.py
@@ -0,0 +1,45 @@
+import random
+
+def hangman(word):
+    wrong = 0
+    stages = ["",
+             "________        ",
+             "|               ",
+             "|        |      ",
+             "|        0      ",
+             "|       /|\     ",
+             "|       / \     ",
+             "|               "
+             ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("Welcome To Hangman..!!!")
+    print("Guess The Programming Language..!!!")
+    name = input("Enter Your Name: ")
+    print("Welcome To Hangman Guessing Game", name + " Good Luck !!!")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter: "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "_" not in board:
+            print("​Congratulations " +"!!!" , name + "You Win")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0: wrong]))
+        print("Sorry " + name + " You lose! It was {}.".format(word))
+
+words = ["python", "java", "kotlin", "javascript", "ruby", "html", "php", "swift", "sql", "go", "dart", "c", "scala", "rust"]
+word = random.choice(words)
+hangman(word)
